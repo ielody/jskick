2be3aa239e000b5fa675cd7a1d300c180f24999a
Ingeborg Eldøy <ieldoy@gmail.com>
2021-10-31 17:15:24 +0100

    sort numbers

---
+++ 22.js
@@ -0,0 +1,9 @@
+// Return the array sorted numbers: [2,3,5,4,6,9,8,7,1] (Array)
+
+const numbers = [2,3,5,4,6,9,8,7,1]
+numbers.sort()
+console.log(numbers)
+
+
+// return: [1,2,3,4,5,6,7,8,9] (Array)
+
