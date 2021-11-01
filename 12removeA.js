432ab046758eb75d7f1d31885a38e1e6729145e0
Ingeborg Eldøy <ieldoy@gmail.com>
2021-11-01 16:00:34 +0100

    trenger hjelp

---
+++ 12removeA.js
@@ -0,0 +1,8 @@
+//remove names that starts with a
+
+const names = "anne,nils,guri,anders,martin"
+
+var nystring = names.split(',').join(', ')
+
+//var result = names.filter('a')
+console.log(nystring)
