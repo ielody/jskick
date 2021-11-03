9219cd420fac4bd18246510c7f7c14321043100f
Ingeborg Eldøy <ieldoy@gmail.com>
2021-11-03 08:10:20 +0100

    trenger hjelp

---
+++ 4loop.js
@@ -0,0 +1,13 @@
+//Return the last five numbers using for loop
+
+//skal denne gjøres om til array så jeg kan skrive ut numrene?
+//Den under skriver i alle fall ut riktig tall.
+
+//string.split()*/
+
+var myStringArray = [1,2,3,4,5,6,7,8,9,10]
+var arrayLength = myStringArray.length
+for (var i = 5; i < arrayLength; i++) {
+    console.log(myStringArray[i])
+}
+// return: "6,7,8,9,10" (String)
