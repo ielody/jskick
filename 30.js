939f68bb9d2b13a7174717424f67b3d06fe25c70
Ingeborg Eldøy <ieldoy@gmail.com>
2021-11-07 19:59:53 +0100

    solved startsWith

---
+++ 30.js
@@ -0,0 +1,8 @@
+//? Skulle denne skrive ut tallet 7 eller "c":7 eller bare c?
+
+//Find object with key equal to 'c'
+// base: [{"a":3},{"b":6},{"c":7}] (Array)
+
+var obj = [{"a":3},{"b":6},{"c":7}]
+console.log(Object.values[3])
+// return: {"c":7} (Object)
