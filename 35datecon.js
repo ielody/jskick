4ec7ab3058135db406dfa7056344a3e20890928a
Ingeborg Eldøy <ieldoy@gmail.com>
2021-11-08 18:45:28 +0100

    ikke ferdig, men provd

---
+++ 35datecon.js
@@ -0,0 +1,18 @@
+//fikk ikke til denne, men du kan i alle fall se forsøk her.
+
+// date: "2019-12-25T07:04:54.275Z" (String)
+
+/*function convertDate(Date) {
+  return Date.parse('25-12-2019').toISOString()
+}
+console.log(convertDate('2019-12-25T07:04:54.275Z'))*/
+// return: "25/12/2019" (String)
+
+/*sfunction convertDate(date) {
+  return date('25/12/2019').toString()
+}
+  console.log(convertDate('2019-12-25T07:04:54.275Z'))
+
+  var datevalue = '2019-12-25T07:04:54.275Z'
+  var converteddate = Date.parse(datevalue)
+  console.log(converteddate)*/
