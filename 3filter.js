4dce6e05d11882b5edb5cd03af796ef124f31668
Ingeborg Eldøy <ieldoy@gmail.com>
2021-11-09 20:54:14 +0100

    solution

--- 3filter.js
+++ 3filter.js
@@ -1,10 +1,10 @@
 // Return the first five numbers using filter
 //values: "1,2,3,4,5,6,7,8,9,10" (String)
-
-function fiveNumbers(values) {
-  return values <= 5
+function fiveNumFilter(values) {
+  return values.split(',').filter((num) => {
+    return num <= 5
+  }).join(',')
 }
-var filtered = [1,2,3,4,5,6,7,8,9,10].filter(fiveNumbers)
+var filtered = fiveNumFilter("1,2,3,4,5,6,7,8,9,10")
 console.log(filtered)
-
-// return: "1,2,3,4,5" (String)
+// return: "1,2,3,4,5" (String)*/
