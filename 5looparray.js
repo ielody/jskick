8f7e596e0b9cd460330428215b435a2d4002a87a
Ingeborg Eldøy <ieldoy@gmail.com>
2021-11-11 21:33:48 +0100

    3 solutions

--- 5looparray.js
+++ 5looparray.js
@@ -1,10 +1,11 @@
 
 // Return the sum of all the numbers using for loop
 
+//1.
 var nums = [2,3,4,5,6,7,8,9]
+var sum = 0
 
 function sumLoop(nums) {
-    var sum = 0
     for (var i = 0; i < nums.length; i++) {
         sum += nums[i]
     }
@@ -12,5 +13,22 @@ function sumLoop(nums) {
 }
 var added = sumLoop(nums)
 console.log(added)
+
+
+//2.
+function sumLoop(nums) {
+    var sum = 0
+    for (const num of nums) {
+        sum += num
+    }
+    return sum.toString()
+}
+var added = sumLoop(nums)
+console.log(added)
+
+
+//3.
+nums.forEach(num => sum += num)
+console.log(sum.toString())
 
 // return: "44" (String)
