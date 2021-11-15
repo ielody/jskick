4c232ecca654a9c72e815fbf50d51620fdf81699
Ingeborg Eldøy <ieldoy@gmail.com>
2021-11-15 22:26:16 +0100

    Solution

---
+++ 21sort.js
@@ -0,0 +1,17 @@
+// Return the array sorted numbers: [2,3,5,4,6,9,8,7,1] (Array)
+
+var numbers = [2,3,5,4,6,9,8,7,1]
+
+function sortNum(numbers) {
+return numbers.sort()
+}
+var result = sortNum(numbers)
+console.log(result)
+
+// return: [1,2,3,4,5,6,7,8,9] (Array)
+
+// function sortNum(numbers) {
+//   return numbers.sort()
+//   }
+//   var result = sortNum([2,3,5,4,6,9,8,7,1])
+//   console.log(result)
