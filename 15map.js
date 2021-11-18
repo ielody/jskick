726b8257fe6afb657da41d0ede11319e94a04d6b
Ingeborg Eldøy <ieldoy@gmail.com>
2021-11-18 09:42:07 +0100

    Solution

--- 15map.js
+++ 15map.js
@@ -1,5 +1,19 @@
-const array1 = [1,2,3,4,5,6,7,8,9,10]
 
-// pass a function to map
-const map1 = array1.map(x => x * 10)
+//Return a new array with numbers multiplied by 10
+
+1.
+
+var array = [1,2,3,4,5,6,7,8,9,10]
+
+function mapArr(array) {
+  return array * 10
+}
+var result = array.map(mapArr)
+console.log(result)
+
+
+
+2.
+const map1 = array.map(x => x * 10)
 console.log(map1)
+
