0870ae8aa4bc8358480c206e8245c075a388908f
Ingeborg Eldøy <ieldoy@gmail.com>
2021-11-18 22:56:09 +0100

    with while

--- 4loop.js
+++ 4loop.js
@@ -14,3 +14,18 @@ var result = fiveNumLoop(values)
 console.log(result)
 
 // return: "6,7,8,9,10" (String)
+
+//eller WHILE
+// var values = "1,2,3,4,5,6,7,8,9,10"
+
+// function fiveNumLoop(values) {
+//     var i = 0
+//     while (values.length > 5) {
+//      i++
+//       return values.split(',').filter((num) => {
+//       return num >  5
+//    }).join(',')
+//   }
+// }
+// var result = fiveNumLoop(values)
+// console.log(result)
