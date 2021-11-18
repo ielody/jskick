8cd7815908ec6b1288e720f0b2fdb5e708bc1a27
Ingeborg Eldøy <ieldoy@gmail.com>
2021-11-18 10:42:17 +0100

    Solution

--- 4loop.js
+++ 4loop.js
@@ -1,13 +1,16 @@
 //Return the last five numbers using for loop
-
-//skal denne gjøres om til array så jeg kan skrive ut numrene?
-//Den under skriver i alle fall ut riktig tall.
-
 //string.split()*/
 
-var myStringArray = [1,2,3,4,5,6,7,8,9,10]
-var arrayLength = myStringArray.length
-for (var i = 5; i < arrayLength; i++) {
-    console.log(myStringArray[i])
+var values = "1,2,3,4,5,6,7,8,9,10"
+
+function fiveNumLoop(values) {
+    for (var i = 0; values.length > 5; i++) {
+      return values.split(',').filter((num) => {
+      return num >  5
+   }).join(',')
+  }
 }
+var result = fiveNumLoop(values)
+console.log(result)
+
 // return: "6,7,8,9,10" (String)
