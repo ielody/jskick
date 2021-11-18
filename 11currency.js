f937a1a9815c23e8ce330507316d8e1fbbebeab6
Ingeborg Eldøy <ieldoy@gmail.com>
2021-11-18 21:07:54 +0100

    Solution

--- 11currency.js
+++ 11currency.js
@@ -1,4 +1,11 @@
 
-var number = 2500;
-console.log(number.toLocaleString('en-US', { style: 'currency', currency: 'USD' }))
+//Return numbers with currency
 
+var values = "1,2,3,4,5,6,7,8,9,10"
+
+function currency(values) {
+  return values.split(',').map(n => '$' + n).join(',')
+}
+console.log(currency(values))
+
+// return: "$1,$2,$3,$4,$5,$6,$7,$8,$9,$10" (String)
