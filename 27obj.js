4c01313173eb3235d9603b700adae63ba733068b
Ingeborg Eldøy <ieldoy@gmail.com>
2021-11-28 12:40:13 +0100

    Solution

--- 27obj.js
+++ 27obj.js
@@ -1,7 +1,12 @@
 //Return all keys that have values larger than 5
 
-const obj = {"a":3,"b":6,"c":7,"d":9,"e":1}
+var obj = {"a":3,"b":6,"c":7,"d":9,"e":1}
 
-var keys = Object.entries(obj).filter(([, v]) => v > 5 ).map(([k]) => k)
+function moreThanFive(obj) {
+  return Object.entries(obj)
+  .filter(([, values]) => values > 5)
+  .map(([keys]) => keys)
+}
+
+console.log(moreThanFive(obj))
 // return: ["b","c","d"] (Array)
-console.log(keys)
