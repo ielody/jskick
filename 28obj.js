64dfc736875681908577c01f6964042c72067d67
Ingeborg Eldøy <ieldoy@gmail.com>
2021-11-28 12:49:53 +0100

    Solution

--- 28obj.js
+++ 28obj.js
@@ -1,9 +1,12 @@
-
 //Return all values larger than 5 and less than 10
 
 var obj = {"a":3,"b":6,"c":7,"d":9,"e":11}
 
-var keys = Object.entries(obj).filter(([, v]) => v > 5 && v < 10).map(([k]) => k)
-console.log(keys)
+function fiveTen(obj) {
+  return Object.entries(obj).filter(([, values]) => values > 5 && values < 10)
+  .map(([keys]) => keys)
+}
+
+console.log(fiveTen(obj))
 
 // return: [6,7,9] (Array)
