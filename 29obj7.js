6fe86b45892d66b4bbc5636748c34a8ccb315c56
Ingeborg Eldøy <ieldoy@gmail.com>
2021-11-28 14:18:19 +0100

    Solution

--- 29obj7.js
+++ 29obj7.js
@@ -1,6 +1,11 @@
-
-
 //Delete all keys larger than 7
+var obj = {"a":3,"b":6,"c":7,"d":9,"e":11}
 
+function largerThanSeven(obj) {
+ Object.keys(obj).map(values =>
+    obj[values] > 7 ? delete obj[values] : values)
+    return obj
+ }
 
+console.log(largerThanSeven(obj))
 // return: {"a":3,"b":6,"c":7} (Object)
