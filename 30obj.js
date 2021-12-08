5a382560ae046e9b9c68b2c3d57c9420363f5f36
Ingeborg Eldøy <ieldoy@gmail.com>
2021-12-08 22:05:41 +0100

    Solution

--- 30obj.js
+++ 30obj.js
@@ -1,9 +1,11 @@
 
-//*Ikke ferdig*
-
 //Find object with key equal to 'c'
-// base: [{"a":3},{"b":6},{"c":7}] (Array)
 
-var obj = [{"a":3},{"b":6},{"c":7}]
-console.log(Object.keys["c"])
-// return: {"c":7} (Object)
+var arr = [{"a":3},{"b":6},{"c":7}]
+
+function equalToC(arr) {
+     return arr.c
+}
+console.log(arr.find(equalToC))
+
+//return {"c":7}
