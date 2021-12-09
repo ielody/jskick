4e850c2891a36164cfe17be40765c1f498303801
Ingeborg Eldøy <ieldoy@gmail.com>
2021-12-09 12:08:33 +0100

    Solution

--- 39flat.js
+++ 39flat.js
@@ -1,5 +1,9 @@
-// numbers: [[1,3],[2,5],[6,9]] (Array)
+// Return the flattened array
 
 var array = [[1,3],[2,5],[6,9]]
-console.log(array.flat());
+
+function flatArr(array) {
+  return array.flat
+}
+console.log(flatArr(array))
 // return: [1,3,2,5,6,9] (Array)
